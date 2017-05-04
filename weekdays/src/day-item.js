624c6ba58cf8d18a8d9a30c1918122701311f264
Momoware <melih@Melihs-MacBook-Pro-2.local>
2017-05-04 17:52:48 +0300

    dinamic styling

--- weekdays/src/day-item.js
+++ weekdays/src/day-item.js
@@ -14,7 +14,26 @@ var DayItem = React.createClass({
         </Text>
     },
     style: function(){
-
+        return{
+            color: this.color(),
+            fontWeight: this.fontWeight(),
+            fontSize: this.fontSize(),
+            lineHeight: this.lineHeight()
+        }
+    },
+    color:function(){
+        var opacity = 10/this.props.daysUntil;
+        return ('rgba(0,0,0, '+opacity+')');
+    },
+    fontWeight:function(){
+        var weight = 7 - this.props.daysUntil;
+        return (weight * 100).toString();;
+    },
+    fontSize: function(){
+        return 60 - 6*this.props.daysUntil;  
+    },
+    lineHeight: function(){
+        return 70 - 4*this.props.daysUntil;  
     }
 });
 
