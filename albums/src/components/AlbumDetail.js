f575d7e641812ca71fdb1f9a953df094e0b60833
Melih Yumak <melih@Melihs-MacBook-Pro-2.local>
2017-07-11 10:38:23 +0300

    albums app finished

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text , View , Image } from 'react-native';
+import { Text , View , Image , Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button'
 
 const AlbumDetail = ({album}) => {
 
-    const {title,artist,thumbnail_image,image} = album;
+    const {title,artist,thumbnail_image,image , url} = album;
     const {headerContentStyle,headerTextStyle,thumbnailStyle,thumbnailContainerStyle,imageStyle} = styles;
 
     return (
@@ -27,7 +27,9 @@ const AlbumDetail = ({album}) => {
                 </Image>
             </CardSection>
             <CardSection>
-                <Button/>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     )
