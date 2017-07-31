a786dacb292d9018a8bd5d1dc47eddb25c32975e
Melih Yumak <melih@Melihs-MacBook-Pro-2.local>
2017-07-31 13:46:07 +0300

    firebase added manager app

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import firebase from 'firebase';
 
-class App extends Component{
-    render(){
-        return(
+class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyBV0pe-hbYla55IjheFcYnHsLZabxYfqFM',
+            authDomain: 'manager-dc477.firebaseapp.com',
+            databaseURL: 'https://manager-dc477.firebaseio.com',
+            projectId: 'manager-dc477',
+            storageBucket: 'manager-dc477.appspot.com',
+            messagingSenderId: '151789716084'
+        };
+        firebase.initializeApp(config);
+    }
+
+    render() {
+        return (
             <Provider store={createStore(reducers)}>
                 <View>
                     <Text>Hello!</Text>
